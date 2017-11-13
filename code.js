55dad964c440f6bd1300953301091b6ee436b7b1
Nick Paolino <github email address>
2017-11-13 11:45:43 -0500

    Updated Code

--- code.js
+++ code.js
@@ -2,10 +2,10 @@ class Code {
   constructor(text){
     this.text = text
 
-    this.dot = 200
-    this.dash = 600
+    this.dot = 150
+    this.dash = 800
     this.pause = 60
-    this.stop = 750
+    this.stop = 800
 
     console.log(text);
 
