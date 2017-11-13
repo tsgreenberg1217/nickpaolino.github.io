eebe6f5cbaa39b36519980c23c07a6004392b8b3
Nick Paolino <github email address>
2017-11-12 21:28:00 -0500

    Added library

--- code.js
+++ code.js
@@ -16,11 +16,30 @@ class Code {
     return {
       "a": [this.dot, this.pause, this.dash, this.stop],
       "b": [this.dash, this.pause, this.dot, this.pause, this.dot, this.pause, this.dot, this.stop],
-      "b": [this.dash, this.pause, this.dot, this.pause, this.dash, this.pause, this.dot, this.stop],
+      "c": [this.dash, this.pause, this.dot, this.pause, this.dash, this.pause, this.dot, this.stop],
+      "d": [this.dash, this.pause, this.dot, this.pause, this.dot, this.stop],
       "e": [this.dot, this.stop],
+      "f": [this.dot, this.pause, this.dot, this.pause, this.dash, this.pause, this.dot, this.stop],
+      "g": [this.dash, this.pause, this.dash, this.pause, this.dot, this.stop],
+      "h": [this.dot, this.pause, this.dot, this.pause, this.dot, this.pause, this.dot, this.stop],
+      "i": [this.dot, this.pause, this.dot, this.stop],
+      "j": [this.dot, this.pause, this.dash, this.pause, this.dash, this.pause, this.dash, this.stop],
+      "k": [this.dash, this.pause, this.dot, this.pause, this.dash, this.stop],
+      "l": [this.dot, this.pause, this.dash, this.pause, this.dot, this.pause, this.dot, this.stop],
+      "m": [this.dash, this.pause, this.dash, this.stop],
+      "n": [this.dash, this.pause, this.dot, this.stop],
       "o": [this.dash, this.pause, this.dash, this.pause, this.dash, this.stop],
+      "p": [this.dot, this.pause, this.dash, this.pause, this.dash, this.pause, this.dot, this.stop],
+      "q": [this.dash, this.pause, this.dash, this.pause, this.dot, this.pause, this.dash, this.stop],
+      "r": [this.dot, this.pause, this.dash, this.pause, this.dot, this.stop],
       "s": [this.dot, this.pause, this.dot, this.pause, this.dot, this.stop],
-      "t": [this.dash, this.stop]
+      "t": [this.dash, this.stop],
+      "u": [this.dot, this.pause, this.dot, this.pause, this.dash, this.stop],
+      "v": [this.dot, this.pause, this.dot, this.pause, this.dot, this.pause, this.dash, this.stop],
+      "w": [this.dot, this.pause, this.dash, this.pause, this.dash, this.stop],
+      "x": [this.dash, this.pause, this.dot, this.pause, this.dot, this.pause, this.dash, this.stop],
+      "y": [this.dash, this.pause, this.dot, this.pause, this.dash, this.pause, this.dash, this.stop],
+      "z": [this.dash, this.pause, this.dash, this.pause, this.dot, this.pause, this.dot, this.stop],
     }
   }
 
